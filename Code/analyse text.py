1c3dcadac64a4ba8c1d55b26ce196249e8c058a3
Seth Wood <tallwoodysw@gmail.com>
2018-03-20 11:02:11 +0000

    Recreating PyPDF2 branch
    Recreating branch from old repository

--- Code/analyse text.py
+++ Code/analyse text.py
@@ -3,26 +3,43 @@ import csv
 import pickle
 import extraforcsv
 import copy
+import PyPDF2
+import sys
 
 
 def Comparing_txt():
-    name = os.getcwd()
+    name = sys.path[0]
+    name = name[:-4]
     master_dic = {}
-    for filename in os.listdir(os.path.join(name, "Text files"))[:len(os.listdir(os.path.join(name, "Text files")))-1]:
-        with open(os.path.join(name, "Text files/{0}".format(filename)), mode='r', errors='ignore') as myfile:
-            data = myfile.read().replace('\n', '')
-            myfile.close()
-            with open(os.path.join(name, "Requisites/Publication_Names.csv"), mode='r', errors='ignore') as csvfile:
+    for filename in os.listdir(os.path.join(name, "PDFs"))[:len(os.listdir(os.path.join(name, "PDFs")))-1]:
+        print(filename)
+        with open(os.path.join(name, "PDFs/{0}".format(filename)), mode='rb') as myfile:
+            pdfReader = PyPDF2.PdfFileReader(myfile)
+            pages = pdfReader.numPages
+            with open(os.path.join(name, "Requisites/Publication_Names.csv"), mode='r', encoding="utf-8") as csvfile:
                 places = csv.DictReader(csvfile)
                 places_dict = {}
                 for row in places:
                     places_dict[row["NP"]] = row["Title"]
 # print(places_dict)
             dic = {}
-        for key, title in places_dict.items():
-            count = data.count(title)
-            if count > 0:
-                dic[key] = count
+            for index in range(pages):
+                print("Page: %d" % index)
+                page = pdfReader.getPage(index)
+                data = page.extractText()
+                for key, title in places_dict.items():
+                    spaces = data.count(title)
+                    nospaces = data.count(title.replace(" ", ""))
+                    if spaces > nospaces:
+                        count = spaces
+                    else:
+                        count = nospaces 
+                    if count > 0:
+                        for frequency in range(count):
+                            if key in dic:
+                                dic[key].append(index)
+                            else:
+                                dic[key] = [index + 1]
         master_dic[filename[filename.find("NP"):filename.find("NP")+4]] = dic
     print(master_dic)
     return master_dic
@@ -33,10 +50,10 @@ def Pickle_Dic(master_dic):
         os.getcwd(), "Requisites/referenceDic.p"), mode="wb"))
 
 
-def Make_CSV(master_dic):
+def PageRef(master_dic):
     # Creates a csv file in the directory of where the program is opened from.
     # To make it the same directory as this script add os.path[0] + "\" to the title
-    with open("csvfile.csv", "w") as csvfile:
+    with open("PageRef.csv", "w") as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(extraforcsv.Titles)
         for dic in master_dic.items():
@@ -53,9 +70,30 @@ def Make_CSV(master_dic):
             csv_writer.writerow(dic_list)
 
 
+def Show_Freq(master_dic):
+    # Creates a csv file in the directory of where the program is opened from.
+    # To make it the same directory as this script add os.path[0] + "\" to the title
+    with open("frequencies.csv", "w") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(extraforcsv.Titles)
+        for dic in master_dic.items():
+            # Deep copy function means that the default line stored in extraforcsv.py is not affected
+            new_line = copy.deepcopy(extraforcsv.line)
+            new_line["Title"] = dic[0]
+            for key, item in dic[1].items():
+                new_line[key] = len(item)
+            dic_list = []
+            # Converts the dictionary used to create the contents of a line into a list
+            # Which is then easily exported into the csv format
+            for title in extraforcsv.Titles:
+                dic_list.append(new_line[title])
+            csv_writer.writerow(dic_list)
+
+            
 if __name__ == "__main__":
     master_dic = Comparing_txt()
-    Make_CSV(master_dic)
+    PageRef(master_dic)
+    Show_Freq(master_dic)
     Pickle_Dic(master_dic)
     print(pickle.load(
         open(os.path.join(os.getcwd(), "Requisites/referenceDic.p"), mode="rb")))
